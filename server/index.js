98124e59a983b0002012c2fd479cdc50dc36a2da
Adrian de Leon <ajdeleon1064@gmail.com>
2018-10-12 23:29:47 -0400

    add test routes connected to postgres

--- server/index.js
+++ server/index.js
@@ -1,13 +1,41 @@
+const keys = require('./keys')
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = new express()
 app.use(cors())
-app.use (bodyParser.json())
+app.use(bodyParser.json())
+
+const { Pool } = require('pg')
+const pgClient = new Pool({
+  port: keys.pgPort,
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+})
+
+pgClient.query('CREATE TABLE IF NOT EXISTS users (id INT, name TEXT)').catch(err => console.log(err))
 
 app.get('/hello', (req, res) => {
   res.send('This is express. Hi.')
+})
+
+app.post('/users', async (req, res) => {
+  const id = req.body.id
+  const name = req.body.name
+
+  pgClient.query('INSERT INTO users(id, name) VALUES($1, $2)', [id, name])
+
+  res.send('Posted successfully')
+})
+
+app.get('/users', async (req, res) => {
+  const values = await pgClient.query('SELECT * from users')
+
+  res.send(values.rows)
 })
 
 app.listen(4000, err => {
