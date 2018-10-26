b6e2a9313cb15b44163f0baa90fe43f1aaa8bf71
Adrian de Leon <ajdeleon1064@gmail.com>
2018-10-25 23:10:56 -0400

    add more redux setup

--- client/src/Workout.js
+++ client/src/Workout.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import TierContainer from './components/TierContainer'
 
-// const Workout = ({lifts}) => (
-//   {lifts.map((lift, i) => {
-//     return <TierContainer tier={i + 1} lift={lift}/>
-//   })}
-// )
-
 const Workout = ({lifts}) => {
   return lifts.map((lift, index) => {
-    return <TierContainer tier={index + 1} lift={lift} />
+    return <TierContainer tier={index + 1} lift={lift} key={lift} />
   })
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'tachyons/css/tachyons.min.css'
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reduxThunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './App'
+import reducers from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  reducers,
+  {},
+  composeEnhancers(applyMiddleware(reduxThunk))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
